2b24f9d81fe2840120f744468a8742429dc051b2
chohh7391 <chohh7391@gmail.com>
2025-10-16 02:13:45 +0900

    modified:   source/wheel_leg_humanoid_lab/wheel_leg_humanoid_lab/tasks/manager_based/wheel_leg_humanoid/velocity/config/walking/agents/rsl_rl_ppo_cfg.py

--- source/wheel_leg_humanoid_lab/wheel_leg_humanoid_lab/tasks/manager_based/wheel_leg_humanoid/velocity/config/walking/agents/rsl_rl_ppo_cfg.py
+++ source/wheel_leg_humanoid_lab/wheel_leg_humanoid_lab/tasks/manager_based/wheel_leg_humanoid/velocity/config/walking/agents/rsl_rl_ppo_cfg.py
@@ -8,12 +8,13 @@ from isaaclab_rl.rsl_rl import RslRlOnPolicyRunnerCfg, RslRlPpoActorCriticCfg, R
 @configclass
 class WheelLegHumanoidWalkingRoughPPORunnerCfg(RslRlOnPolicyRunnerCfg):
     num_steps_per_env = 24
-    max_iterations = 200000
-    save_interval = 1000
-    experiment_name = "wheel_leg_humanoid/walking/rough"
-    empirical_normalization = False
+    max_iterations = 3000
+    save_interval = 50
+    experiment_name = "walking/rough"
     policy = RslRlPpoActorCriticCfg(
         init_noise_std=1.0,
+        actor_obs_normalization=False,
+        critic_obs_normalization=False,
         actor_hidden_dims=[512, 256, 128],
         critic_hidden_dims=[512, 256, 128],
         activation="elu",
@@ -22,7 +23,7 @@ class WheelLegHumanoidWalkingRoughPPORunnerCfg(RslRlOnPolicyRunnerCfg):
         value_loss_coef=1.0,
         use_clipped_value_loss=True,
         clip_param=0.2,
-        entropy_coef=0.01,
+        entropy_coef=0.008,
         num_learning_epochs=5,
         num_mini_batches=4,
         learning_rate=1.0e-3,
@@ -39,5 +40,5 @@ class WheelLegHumanoidWalkingFlatPPORunnerCfg(WheelLegHumanoidWalkingRoughPPORun
     def __post_init__(self):
         super().__post_init__()
 
-        self.max_iterations = 100000
-        self.experiment_name = "wheel_leg_humanoid/walking/flat"
+        self.max_iterations = 1500
+        self.experiment_name = "walking/flat"
