022ed4955e8ec6ff831007572d499351c37e3c47
chohh7391 <chohh7391@gmail.com>
2026-01-05 11:54:03 +0900

    수정함:        source/wheel_leg_humanoid_lab/wheel_leg_humanoid_lab/tasks/manager_based/wheel_leg_humanoid/velocity/config/driving/agents/rsl_rl_ppo_cfg.py 	수정함:        source/wheel_leg_humanoid_lab/wheel_leg_humanoid_lab/tasks/manager_based/wheel_leg_humanoid/velocity/config/walking/agents/rsl_rl_ppo_cfg.py

--- source/wheel_leg_humanoid_lab/wheel_leg_humanoid_lab/tasks/manager_based/wheel_leg_humanoid/velocity/config/driving/agents/rsl_rl_ppo_cfg.py
+++ source/wheel_leg_humanoid_lab/wheel_leg_humanoid_lab/tasks/manager_based/wheel_leg_humanoid/velocity/config/driving/agents/rsl_rl_ppo_cfg.py
@@ -10,7 +10,7 @@ from isaaclab_rl.rsl_rl import RslRlOnPolicyRunnerCfg, RslRlPpoActorCriticCfg, R
 class WheelLegHumanoidDrivingRoughPPORunnerCfg(RslRlOnPolicyRunnerCfg):
     num_steps_per_env = 24
     max_iterations = 20000
-    save_interval = 200
+    save_interval = 500
     experiment_name = "driving/rough"
     policy = RslRlPpoActorCriticCfg(
         init_noise_std=1.0,
